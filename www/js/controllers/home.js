4ab52925f270902a2bb71e4b6eecd1616f6a305a
Guchi <gustavolunar@gmail.com>
2016-11-29 08:27:11 -0600

    ListAllMatches

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,16 +1,28 @@
 'use strict';
 
-app.controller('HomeCtrl', function(Auth, $scope, Like, uid) {
+app.controller('HomeCtrl', function(Auth, $ionicLoading, $scope, Like, Match, uid) {
 
 	var home = this;
 	home.currentIndex = null;
+	home.currentCardUid = null; 
 
 	var maxAge = null;
 	var men = null; 
 	var women = null;
 	var currentUid = uid; 
 
+	$scope.show = function() {
+		$ionicLoading.show({
+			template: '<ion-spinner icon="bubbles"></ion-spinner>'
+		});
+	}; 
+
+	$scope.hide = function() {
+		$ionicLoading.hide();
+	}
+
 	function init() {
+		$scope.show();
 		home.profiles = [];
 
 		maxAge = JSON.parse(window.localStorage.getItem('maxAge')) || 25;
@@ -26,18 +38,28 @@ app.controller('HomeCtrl', function(Auth, $scope, Like, uid) {
 				var item = data[i];
 
 				if((item.gender == 'male' && men) || (item.gender == 'female' && women)) {
+					if (item.$id != currentUid)
 					home.profiles.push(item);
 				}
 
 			}
 
+			Like.allLikesByUser(currentUid).$loaded().then(function(likesList) {
+				home.profiles = _.filter(home.profiles, function(obj) {
+					return _.isEmpty(_.where(likesList, {$id: obj.$id}));
+				});
+			});
+
 			if (home.profiles.length > 0) {
 				home.currentIndex = home.profiles.length - 1; 
+				home.currentCardUid = home.profiles[home.currentIndex].$id;
 
 			}
 
+			$scope.hide();
+
 		}); 
-	}
+	}; 
 
 
 	$scope.$on('$ionicView.enter', function(e) {
@@ -51,6 +73,8 @@ app.controller('HomeCtrl', function(Auth, $scope, Like, uid) {
 
 	home.like = function(index, like_uid) {
 		Like.addLike(currentUid, like_uid);
+		Match.checkMatch(currentUid, like_uid);
+		
 		home.cardRemoved(index);
 		console.log('Like');
 	};
@@ -60,9 +84,11 @@ app.controller('HomeCtrl', function(Auth, $scope, Like, uid) {
 
 		if (home.profiles.length > 0) {
 				home.currentIndex = home.profiles.length - 1; 
+				home.currentCardUid = home.profiles[home.currentIndex].$id;
+
 
 			}
-	}
+	};
 
 
 
